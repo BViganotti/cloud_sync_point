e885832a0848e7b3b9db523225d64dba654c061a
BViganotti <viganotti.exe@gmail.com>
2024-07-28 11:12:28 +0200

    added prints + formatting

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ async fn main() {
 
     let endpoints = post_endpoint.or(get_endpoint);
 
-    println!("Cloud sync-point running at: 127.0.0.1:3030");
+    println!("cloud sync-point running at: 127.0.0.1:3030");
     warp::serve(endpoints).run(([127, 0, 0, 1], 3030)).await;
 }
 
@@ -46,29 +46,34 @@ async fn request_handler(
     let (tx, rx) = oneshot::channel();
 
     {
-        // make data safe to access
+        // make state map data safe to access
         let mut req_state_guard = req_state.lock().unwrap();
         // we try to remove the unique_id key we got from the map, if we succeed (meaning sender won't be None),
         // that mean the second party requested our uri
         if let Some(sender) = req_state_guard.remove(&unique_id) {
             // we have our second party, let's unblock and reply OK
             sender.send(()).ok();
+            //println!("The second party requested the URI");
             return Ok(warp::reply::with_status(
-                "The second party requested the URI",
+                "the second party requested the URI",
                 warp::http::StatusCode::OK,
             ));
         } else {
             // the unique_id wasn't in the map, that means it is just the first party requesting the uri
             // this makes us able to send a signal to the second party when it will request the uri
+            println!("the first party requested the URI");
             req_state_guard.insert(unique_id, tx);
         }
     }
 
     tokio::select! {
-        _ = rx => Ok(warp::reply::with_status("The second party requested the URI", warp::http::StatusCode::OK)),
+        _ = rx => {
+            println!("the second party requested the URI");
+            Ok(warp::reply::with_status("the second party requested the URI", warp::http::StatusCode::OK))},
         // if this complete, it means that the second party didn't request the uri in time
         _ = tokio::time::sleep(Duration::from_secs(10)) => {
             // locking the map before removing the state
+            println!("the second party did not request the URI, reponding with TIMEOUT");
             let mut req_state_guard = req_state.lock().unwrap();
             req_state_guard.remove(&unique_id);
             Ok(warp::reply::with_status("TIMEOUT", warp::http::StatusCode::REQUEST_TIMEOUT))
--- test.go
+++ test.go
@@ -4,42 +4,42 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"time"
 )
 
-func main() {
-	// Endpoint URL
-	url := "http://127.0.0.1:3030/wait-for-second-party/F331860E-7745-49B4-8A0F-74249E954658" // generate the uuid dynamically
-
-	// some data
-	var jsonStr = []byte(`{"Hire_me":"Please"}`)
-
-	// fire first post req
-	sendPostRequest(url, jsonStr)
-
-	time.Sleep(5 * time.Second)
-
-	// fire second req
-	sendPostRequest(url, jsonStr)
-}
-
-func sendPostRequest(url string, jsonStr []byte) {
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+func makePostRequest(url string, data []byte) {
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
 	if err != nil {
-		log.Fatalf("Error creating request: %v", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Error sending request: %v", err)
+		fmt.Println("error making request:", err)
+		return
 	}
 	defer resp.Body.Close()
+	// read the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("error reading response body:", err)
+		return
+	}
+	// print the response
+	fmt.Println("response:", string(body))
+}
 
-	body, _ := io.ReadAll(resp.Body)
-	fmt.Printf("Response status: %s\n", resp.Status)
-	fmt.Printf("Response body: %s\n", body)
+func main() {
+	url := "http://127.0.0.1:3030/wait-for-second-party/F331860E-7745-49B4-8A0F-74249E954658"
+	data := []byte(`{"Hire_me":"Please"}`)
+
+	// we fire the second POST request in a different goroutine to not block the script
+	go func() {
+		fmt.Println("firing second POST request in 5s")
+		time.Sleep(5 * time.Second)
+		makePostRequest(url, data)
+	}()
+
+	// firing the first POST request to the URI
+	fmt.Println("sending first POST request")
+	makePostRequest(url, data)
+
+	// wait to ensure the second request completes before the script exits
+	time.Sleep(6 * time.Second)
 }
